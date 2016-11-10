157c741790049492a45fb7a4bb45c39705e14cad
Richard Livsey <richard@livsey.org>
2016-11-10 17:12:08 +0000

    display time and duration for messages

--- src/components/channel-info/component.js
+++ src/components/channel-info/component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import './styles.css';
 
 export default function(props) {
@@ -9,16 +10,31 @@ export default function(props) {
           <tr>
             <th>Ref</th>
             <th>Event</th>
+            <th>Time</th>
+            <th>Duration</th>
             <th>Payload</th>
           </tr>
         </thead>
         <tbody>
           {props.messages.map(message => {
             const key = `${message.ref}-${message.event}`;
+            const time = moment(message.time);
+
+            const original = findOriginalFor(props.messages, message);
+            let duration;
+            if (original) {
+              const diff = time.diff(moment(original.time));
+              duration = `${diff}ms`;
+            } else {
+              duration = "-";
+            }
+
             return (
               <tr key={key} className="message">
                 <td>{message.ref}</td>
                 <td>{message.event}</td>
+                <td>{time.format("HH:mm:ss.SSS")}</td>
+                <td>{duration}</td>
                 <td>{JSON.stringify(message.payload)}</td>
               </tr>
             )
@@ -27,4 +43,12 @@ export default function(props) {
       </table>
     </div>
   )
+}
+
+function findOriginalFor(messages, message) {
+  if (message.event !== "phx_reply") {
+    return;
+  }
+
+  return messages.find(m => m.ref === message.ref && m !== message);
 }
