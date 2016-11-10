4c0f045c3fa527f6d73a08fbca5f6ffbc5a4c026
Richard Livsey <richard@livsey.org>
2016-11-10 16:11:08 +0000

    Don’t overflow channel list & info

--- src/components/channel-list/component.js
+++ src/components/channel-list/component.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import './styles.css';
 
-export default class ChannelList extends Component {
+export default function(props) {
+  return <ul className="channel-list">
+    {sortBy("topic", props.channels).map(channel => {
+      const messages = props.messages[channel.topic] || [];
+      const itemClass = classNames({
+        "channel-list-item": true,
+        selected: props.selected && props.selected.topic === channel.topic
+      });
 
-  selectChannel(channel) {
-    this.props.onSelect(channel);
-  }
+      return <li className={itemClass} key={channel.topic} onClick={(e) => props.onSelect(channel)}>
+        {channel.topic}
+      </li>
+    })}
+  </ul>
+}
 
-  render() {
-    return <ul className="channel-list">
-      {this.props.channels.map(channel => {
-        const messages = this.props.messages[channel.topic] || [];
-        const itemClass = classNames({
-          "channel-list-item": true,
-          selected: this.props.selected && this.props.selected.topic === channel.topic
-        });
+function sortBy(key, array) {
+  return array.sort((a, b) => {
+    const aValue = a[key];
+    const bValue = b[key];
 
-        return <li className={itemClass} key={channel.topic} onClick={(e) => this.selectChannel(channel)}>
-          {channel.topic} ({messages.length})
-        </li>
-      })}
-    </ul>
-  }
-
+    if (aValue < bValue) {
+      return -1;
+    } else if (aValue > bValue) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 }
