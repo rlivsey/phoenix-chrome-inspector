295cea00bfe306257568f08a6c92e5ffeb545a5d
Richard Livsey <richard@livsey.org>
2016-11-15 17:12:42 +0000

    Generate a random payload

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -4,11 +4,23 @@ export const CHANNELS = TOPICS.map(topic => ({
   topic, state: Math.random() > 0.75 ? "open" : "closed"
 }));
 
+
+function randomPayload(depth=0) {
+  let payload = {};
+
+  const numKeys = Math.floor(Math.random() * 10);
+  for (let i=0; i<numKeys; i++) {
+    payload[`key${i}`] = depth < 3 && Math.random() < 0.25 ? randomPayload(depth + 1) : Math.random();
+  }
+
+  return payload;
+}
+
 export const MESSAGES = (function() {
   let time = new Date();
   let total = 100;
 
-  const messages = {};
+  let messages = {};
   TOPICS.forEach(topic => {
     messages[topic] = [];
   });
@@ -19,7 +31,7 @@ export const MESSAGES = (function() {
     let topic = TOPICS[Math.floor(Math.random() * TOPICS.length)];
     messages[topic].push({
       event: "some:event",
-      payload: {foo: true, ref},
+      payload: randomPayload(),
       ref,
       time,
       topic
